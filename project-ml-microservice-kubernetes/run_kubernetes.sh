4b6cc6e6accc08551f20e80b65f155d45ba68c3c
Sabina Khanal <sabina.khanal@cloudwick.com>
2022-01-23 21:44:19 +0000

    task 7

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=sabs3/udacity-project
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+docker login
+kubectl run udacity-project --image=$dockerpath --port=80 --labels app=udacity-project
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward udacity-project 8000:80
 
